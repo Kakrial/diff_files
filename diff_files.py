28c1662bfbfaf46ecef770f3765fdec4fa92ad8a
kakrial <kristian.fiebig@gmail.com>
2020-09-25 22:13:46 +1000

    Added percentage completion

--- diff_files.py
+++ diff_files.py
@@ -1,19 +1,19 @@
 import sys
 import os
+import myUtils as mu
 
 def missingFiles(dir1, file):
-    dirPath = os.path.relpath(dir1)
-    filePath = os.path.relpath(file)
-    if not os.path.exists(dirPath):
-        print(f"{dirPath}: directory doesn't exist")
+    dirPath = mu.getPath(dir1)
+    if dirPath is None:
         return
-    if not os.path.exists(filePath):
-        print(f"{filePath}: directory doesn't exist")
+    filePath = mu.getPath(file)
+    if filePath is None:
         return
+
     dirFiles = os.listdir(dirPath)
     with open(filePath, 'r') as f:
         fFiles = f.read().splitlines()
-    
+
     print("Following files missing in directory:\n")
     missingFiles = 0
     matchedFiles = 0
@@ -23,24 +23,14 @@ def missingFiles(dir1, file):
             continue
         if f.startswith("#"):
             continue
-        # if not dirFiles.__contains__(f):
-        #     print(f)
-        #     missingFiles += 1
-        if not containsPrefix(f, dirFiles):
+        if not mu.containsPrefix(f, dirFiles):
             print(f)
             missingFiles += 1
         else:
             matchedFiles += 1
     
-    print(f"\nMatched Files:\t{matchedFiles}\nMissing Files:\t{missingFiles}\nTotal Files:\t{matchedFiles+missingFiles}")
+    print(f"\nMatched Files:\t{matchedFiles}\nMissing Files:\t{missingFiles}\nPercentage:\t{(int)(matchedFiles/(matchedFiles+missingFiles)*100)}%\nTotal Files:\t{matchedFiles+missingFiles}")
 
-def containsPrefix(value: str, cmpList:list) -> bool:
-    pref = value.split(".")[0]
-    val: str
-    for val in cmpList:
-        if val.startswith(pref):
-            return True
-    return False
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
